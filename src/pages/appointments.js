5531eec15b1c23dc34fa250605396d9731917741
Jack Storment <jstorment1998@gmail.com>
2020-08-16 07:37:53 -0400

    Update appointments.js

--- src/pages/appointments.js
+++ src/pages/appointments.js
@@ -19,7 +19,7 @@ export const AppointmentsPageTemplate = () => {
                 Facebook page. The available schedule for Eastern Air are in the calendar below:
               </p>
               <a className="button" href="https://fb.com/book/Eastern-Air-109819697434099" style={{marginTop: 20, marginBottom: 20}}>
-                Schedule an Appointment
+                Schedule an Appointment with Eastern Air
               </a>
               <Iframe url="https://calendar.google.com/calendar/embed?height=800&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=Z2FpdGtlZXBlcjEzQGdtYWlsLmNvbQ&amp;src=aTBpMjQ2M3EzY2xrM2RqMHRvczc3bjF0OTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%237986CB&amp;color=%23F6BF26&amp;color=%2333B679&amp;color=%230B8043" styles={{borderWidth: 0}} frameborder="0" height="800" width="100%"/>
             </div>
